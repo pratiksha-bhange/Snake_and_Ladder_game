bb9c6861223de8fa9c76f6324612678219d1d2f5
pratiksha-bhange <108232994+pratiksha-bhange@users.noreply.github.com>
2022-09-18 14:26:32 +0530

    [Pratu]ADDED: Player Gets  Exact Winnig Position 100

--- Snake and Ladder Game/Program.cs
+++ Snake and Ladder Game/Program.cs
@@ -14,31 +14,27 @@ namespace Snake_and_Ladder_Game
             int position = 0;
             Random random = new Random();
 
-            int dieCount = 0;
             while (position != 100)
             {
                 int noOnDie = random.Next(1, 7);
                 Console.WriteLine($"No on die : {noOnDie}");
-                dieCount++;
-                int options = random.Next(1, 4);
 
+                int options = random.Next(1, 4);
                 switch (options)
                 {
                     case LADDER:
                         position += noOnDie;
-                        position = position > 100 ? position -= noOnDie : position;
+                        position = position < 0 ? 0 : position;
                         break;
-
                     case SNAKE:
                         position -= noOnDie;
                         //turnery operator
                         position = position < 0 ? 0 : position;
                         break;
-
                     case NOPLAY:
+                       
                         break;
                 }
-                Console.WriteLine($" die count:{dieCount} Position: {position}");
             }
             Console.WriteLine("POSITION " + position);
         }
