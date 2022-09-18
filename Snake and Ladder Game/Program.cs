3de27deeb5fbf1ed3fe11f691df6e7bfe3a77123
pratiksha-bhange <pratikshabhange101@gmail.com>
2022-09-18 13:04:04 +0530

    [Pratu]ADDED: Calculate Daily Employee Wage

--- Snake and Ladder Game/Program.cs
+++ Snake and Ladder Game/Program.cs
@@ -6,8 +6,25 @@ namespace Snake_and_Ladder_Game
     {
         static void Main(string[] args)
         {
-            int position = 0;
-            Console.WriteLine(" Player at start position : " + position);
+            Console.WriteLine("Welcome to employee wage computation program");
+
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee wages is=" + empWage);
         }
     }
+    
 }
